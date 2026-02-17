2d03a9b89cbb0743e26757eacf27789bff832c03
niishaaaa5121 <niishaaaa51.21@gmail.com>
2026-02-17 16:10:05 +0530

    Modify text and comments in p02.py
    Updated the text spoken by the engine and improved comments.

--- p02.py
+++ p02.py
@@ -4,7 +4,8 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("i will speak this text")
+engine.say("this is a text to specch feature, it was coded with the help of copilot")
 engine.runAndWait()
+
 
 # this is how we can use pyttsx3 module to convert text to speech in python
