411277440f082bd1b719071a0a0a925267992eba
niishaaaa5121 <niishaaaa51.21@gmail.com>
2026-02-17 16:12:30 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,7 @@
+a = input("Enter number 1: ")
+b = input("Enter number 2: ")
+print("Number a is:", a)
+print("Number b is:", b)
+# the input() function is used to take input from the user in python. It prompts the user to enter a value and returns it as a string. In this example, we are asking the user to enter two values and then printing them out. The values entered by the user will be stored in the variables a and b, and we can use them later in our program if needed.
+
+# to copy the same line down do shift+alt+down arrow key
